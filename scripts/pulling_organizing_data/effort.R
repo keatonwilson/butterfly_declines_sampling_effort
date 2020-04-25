92364f5c0ddefa8beef167e21324f72259166132
keatonwilson <keatonwilson@me.com>
2020-04-25 07:25:23 -0700

    inat_data incorporation from gbif download - calculating effort by year

---
+++ scripts/pulling_organizing_data/effort.R
@@ -0,0 +1,52 @@
+# Slimming and filtering inat data
+# Keaton Wilson
+# keatonwilson@me.com
+# 2020-04-25
+
+# libraries
+library(tidyverse)
+library(lubridate)
+
+# reading in data
+inat_data = read_tsv("./data/inat_data/occurrence.txt")
+
+# slimming down
+state_list = c("California", "Oregon", "Washington", "Idaho", "Montana", 
+               "Wymoning", "Nevada", "Utah", "Arizona", "New Mexico", 
+               "Colorado")
+inat_slim = inat_data %>%
+  select(identifier, recordedBy, scientificName, 
+         eventDate, countryCode, stateProvince, 
+         decimalLatitude, decimalLongitude) %>%
+  filter(countryCode == "US", stateProvince %in% state_list, 
+         year(eventDate) >= 1985 & year(eventDate) < 2020) %>%
+  drop_na()
+
+inat_slim %>%
+  summarize(earliest_date = min(eventDate), 
+            recent_date = max(eventDate))
+
+# generating a unique combination of user-day efforts 
+# (general, not location specific)
+# 
+effort_summary = inat_slim %>%
+  select(recordedBy, eventDate) %>%
+  transmute(date = date(eventDate), 
+            recordedBy = recordedBy) %>%
+  distinct() %>%
+  mutate(year = year(date)) %>%
+  group_by(year) %>%
+  summarize(year_total_person_days = n()) 
+
+
+effort_summary %>%
+  ggplot(aes(x = year, y = year_total_person_days)) +
+  geom_line()
+
+write_csv(effort_summary, path = "./data/effort_summary.csv")
+
+
+
+
+
+
