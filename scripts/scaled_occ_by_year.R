6e4bb56e43dd6cb2794f2c647049097e960d9913
keatonwilson <keatonwilson@me.com>
2020-04-29 09:46:00 -0700

    changed scaled_occ_by_year to output only iNaturalist occurences, omitting gbif records.

--- scripts/scaled_occ_by_year.R
+++ scripts/scaled_occ_by_year.R
@@ -9,10 +9,13 @@ library(lubridate)
 
 # importing data
 effort = read_csv("./data/effort_summary.csv")
-occ = read_csv("./data/candidate_occurences.csv")
+occ = read_csv("./data/full_occs.csv")
+
+occ_inat = occ %>%
+  filter(prov == "inat")
 
 # joining
-joined_occ = occ %>%
+joined_occ = occ_inat %>%
   mutate(year = year(date)) %>%
   filter(year >= 1985 & year < 2020) %>%
   group_by(year, name) %>%
@@ -27,8 +30,13 @@ g1 = ggplot(joined_occ, aes(x = year, y = index)) +
   theme_classic() +
   geom_smooth(method = "lm") +
   facet_wrap(~ name) +
-  ylab("Yearly total occurences/number of bserver days per year") +
+  ylab("Yearly total occurences/number of observer days per year") +
   xlab("Year") +
   theme(strip.text = element_text(face = "italic"))
 
 ggsave(filename = "./output/scaled_occ_by_year.png", plot = g1)
+
+joined_occ = joined_occ %>%
+  rename(counts = n, effort = year_total_person_days, 
+         effort_scaled_counts = index)
+write_csv(joined_occ, path = "./output/effort_scaled_counts.csv")
